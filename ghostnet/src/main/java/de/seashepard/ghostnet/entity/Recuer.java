f3b75f8c89bbbf9059bfa53e05ee14dd0f314e25
Kassy-tech11 <kassandra.mucks@gmail.com>
2026-06-01 18:18:01 +0200

    Create Rescuer entity with id, name, and phoneNumber

--- ghostnet/src/main/java/de/seashepard/ghostnet/entity/Recuer.java
+++ ghostnet/src/main/java/de/seashepard/ghostnet/entity/Recuer.java
@@ -1,1 +1,34 @@
+package de.sheasepherd.ghostnet.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+public class Rescuer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String phoneNumber;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
